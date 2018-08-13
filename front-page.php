192aa719e31effe9cafc2a665887d44bf1b0916a
juliesof <juliesof@gmail.com>
2018-08-13 17:40:20 +0200

    First commit of understrap files along with front-page

---
+++ front-page.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * The template for displaying the front page.
+ *
+ *
+ * @package understrap
+ */
+
+get_header();
+
+$container   = get_theme_mod( 'understrap_container_type' );
+
+?>
+
+	<div class="container-fluid" id="content-frontpage" tabindex="-1">
+
+		<div class="row">
+
+			<main class="site-main" id="main">
+
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<?php get_template_part( 'loop-templates/frontpage', 'hero' ); ?>
+					<?php get_template_part( 'loop-templates/frontpage', 'welcome' ); ?>
+					<?php get_template_part( 'loop-templates/frontpage', 'services' ); ?>
+					<?php get_template_part( 'loop-templates/frontpage', 'testimonials' ); ?>
+					<?php get_template_part( 'loop-templates/frontpage', 'gallery' ); ?>
+
+				<?php endwhile; // end of the loop. ?>
+
+			</main><!-- #main -->
+
+	</div><!-- .row -->
+
+</div><!-- Container end -->
+
+<?php get_footer(); ?>
---
+++ loop-templates/frontpage-services.php
@@ -0,0 +1,22 @@
+<!-- 
+	List of services
+
+	New Construction
+	Restoration & Renovation
+	Architectural Detective Work
+	Green Practices
+	Real Estate Purchase Consultation
+
+ -->
+
+<div id="homepage-services" class="row">
+	<div class="masonry-layout">
+		<div class="masonry-layout__column">
+			<div class="masonry-layout__panel">
+				<div class="masonry-layout__panel-content">
+					<!-- Content Here -->
+				</div> <!-- end __panel-content -->
+			</div> <!-- end _panel -->
+		</div> <!-- end _column -->
+	</div> <!-- end masonry-layout -->
+</div>
