721037154f7274e1ff6f3a50c6baca920f3d7d87
abufrank <riley.thornton1@gmail.com>
2018-08-14 13:53:58 -0400

    fixed stylesheet enqueue

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -18,7 +18,7 @@ if ( ! function_exists( 'understrap_scripts' ) ) {
 		wp_enqueue_style( 'understrap-styles', get_stylesheet_directory_uri() . '/css/theme.min.css', array(), $css_version );
 
 		wp_enqueue_style( 'creative-styles',
-			get_stylesheet_directory_uri() . '/css/style-creative.css', array(), 1, true );
+			get_stylesheet_directory_uri() . '/css/style-creative.css', array(), 1 );
 
 		wp_enqueue_script( 'jquery');
 		wp_enqueue_script( 'popper-scripts', get_template_directory_uri() . '/js/popper.min.js', array(), $theme_version, true);
