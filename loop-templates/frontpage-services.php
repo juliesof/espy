56d71e8c1fe278dae70bfbb2b9719920cdf69b16
juliesof <juliesof@gmail.com>
2018-08-14 15:50:37 +0200

    Beginning of Services Masonry layout with Flexbox

--- loop-templates/frontpage-services.php
+++ loop-templates/frontpage-services.php
@@ -9,12 +9,49 @@
 
  -->
 
-<div id="homepage-services" class="row">
+<div id="homepage-services">
+	<div class="text-center">
+		<h2>Our Specialities</h2>
+	</div>
+	<!-- Begin Masonry Layout -->
 	<div class="masonry-layout">
-		<div class="masonry-layout__column">
+		<div id="new-construction" class="masonry-layout__column">
 			<div class="masonry-layout__panel">
 				<div class="masonry-layout__panel-content">
-					<!-- Content Here -->
+					<h2>New Construction</h2>
+					<div>A thin paint will stick to a thick paint. Let that brush dance around there and play. A beautiful little sunset. Look around, look at what we have. Beauty is everywhere, you only have to look to see it. I'm sort of a softy, I couldn't shoot Bambi except with a camera. We have all at one time or another mixed some mud.</div>
+				</div> <!-- end __panel-content -->
+			</div> <!-- end _panel -->
+		</div> <!-- end _column -->
+		<div id="restoration" class="masonry-layout__row">
+			<div class="masonry-layout__panel--2">
+				<div class="masonry-layout__panel-content">
+					<h2>Restoration and Renovation</h2>
+					<div>A thin paint will stick to a thick paint. Let that brush dance around there and play. A beautiful little sunset. Look around, look at what we have. Beauty is everywhere, you only have to look to see it. I'm sort of a softy, I couldn't shoot Bambi except with a camera. We have all at one time or another mixed some mud.</div>
+				</div> <!-- end __panel-content -->
+			</div> <!-- end _panel -->
+		</div> <!-- end row -->
+		<div id="detective" class="masonry-layout__row">
+			<div class="masonry-layout__panel--1">
+				<div class="masonry-layout__panel-content">
+					<h2>Architectural Detective Work</h2>
+					<div>A thin paint will stick to a thick paint. Let that brush dance around there and play. A beautiful little sunset. Look around, look at what we have. Beauty is everywhere, you only have to look to see it. I'm sort of a softy, I couldn't shoot Bambi except with a camera. We have all at one time or another mixed some mud.</div>
+				</div> <!-- end __panel-content -->
+			</div> <!-- end _panel -->
+		</div> <!-- end row -->
+		<div id="green" class="masonry-layout__column">
+			<div class="masonry-layout__panel">
+				<div class="masonry-layout__panel-content">
+					<h2>Green Practices</h2>
+					<div>A thin paint will stick to a thick paint. Let that brush dance around there and play. A beautiful little sunset. Look around, look at what we have. Beauty is everywhere, you only have to look to see it. I'm sort of a softy, I couldn't shoot Bambi except with a camera. We have all at one time or another mixed some mud.</div>
+				</div> <!-- end __panel-content -->
+			</div> <!-- end _panel -->
+		</div> <!-- end _column -->
+		<div id="purchase-consult" class="masonry-layout__row">
+			<div class="masonry-layout__panel--2">
+				<div class="masonry-layout__panel-content">
+					<h2>Real Estate Purchase Consultation</h2>
+					<div>A thin paint will stick to a thick paint. Let that brush dance around there and play. A beautiful little sunset. Look around, look at what we have. Beauty is everywhere, you only have to look to see it. I'm sort of a softy, I couldn't shoot Bambi except with a camera. We have all at one time or another mixed some mud.</div>
 				</div> <!-- end __panel-content -->
 			</div> <!-- end _panel -->
 		</div> <!-- end _column -->
