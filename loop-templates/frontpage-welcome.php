6e38d045f129253f52cf7bc982c76f82319ce697
juliesof <juliesof@gmail.com>
2018-08-14 17:16:51 +0200

    small changes to welcome section

--- loop-templates/frontpage-welcome.php
+++ loop-templates/frontpage-welcome.php
@@ -1,6 +1,5 @@
-<div id="homepage-welcome" class="row">
-	<div class="col-md-5 offset-md-4">	
-		<div id="homepage-welcome-text">You can do it. I sincerely wish for you every possible joy life could bring. And that's when it becomes fun - you don't have to spend your time thinking about what's happening - you just let it happen.
-		</div>
+<div id="homepage-welcome">
+	<div id="homepage-welcome-text">
+		You can do it. I sincerely wish for you every possible joy life could bring. And that's when it becomes fun - you don't have to spend your time thinking about what's happening - you just let it happen.
 	</div>
 </div>
