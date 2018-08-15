36d86d38c2c3c4e00e8eb70dbdaa2c92f884012e
juliesof <juliesof@gmail.com>
2018-08-15 13:59:41 +0200

    basic layout for homepage testimonials section

--- front-page.php
+++ front-page.php
@@ -14,24 +14,20 @@ $container   = get_theme_mod( 'understrap_container_type' );
 
 	<div class="container-fluid" id="content-frontpage" tabindex="-1">
 
-		<div class="row">
+		<main class="site-main" id="main">
 
-			<main class="site-main" id="main">
+			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php while ( have_posts() ) : the_post(); ?>
+				<?php get_template_part( 'frontpage-templates/frontpage', 'hero' ); ?>
+				<?php get_template_part( 'frontpage-templates/frontpage', 'welcome' ); ?>
+				<?php get_template_part( 'frontpage-templates/frontpage', 'services' ); ?>
+				<?php get_template_part( 'frontpage-templates/frontpage', 'testimonials' ); ?>
+				<?php get_template_part( 'frontpage-templates/frontpage', 'gallery' ); ?>
 
-					<?php get_template_part( 'frontpage-templates/frontpage', 'hero' ); ?>
-					<?php get_template_part( 'frontpage-templates/frontpage', 'welcome' ); ?>
-					<?php get_template_part( 'frontpage-templates/frontpage', 'services' ); ?>
-					<?php get_template_part( 'frontpage-templates/frontpage', 'testimonials' ); ?>
-					<?php get_template_part( 'frontpage-templates/frontpage', 'gallery' ); ?>
+			<?php endwhile; // end of the loop. ?>
 
-				<?php endwhile; // end of the loop. ?>
-
-			</main><!-- #main -->
-
-	</div><!-- .row -->
-
+		</main><!-- #main -->
+			
 </div><!-- Container end -->
 
 <?php get_footer(); ?>
---
+++ frontpage-templates/frontpage-testimonials.php
@@ -0,0 +1,15 @@
+<h2 class="text-center">Words from our Clients</h2>
+<div id="homepage-testimonials-layout" class="row">
+	<!-- Testimonial 1 -->
+<!-- 	<div class="col-md-6">
+ -->		<div class="homepage-testimonial">
+			Work that paint. We'll lay all these little funky little things in there. Imagination is the key to painting. Decide where your cloud lives. Maybe he lives right in here. We tell people sometimes: we're like drug dealers, come into town and get everybody absolutely addicted to painting. It doesn't take much to get you addicted.
+		</div>
+<!-- 	</div>
+ -->	<!-- Testimonial 2 -->
+<!-- 	<div class="col-md-6">
+ -->		<div class="homepage-testimonial">
+			This is probably the greatest thing to happen in my life - to be able to share this with you. There are no limits in this world. Let's get wild today.
+		</div>
+<!-- 	</div>
+ --></div>
