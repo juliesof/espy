bb4eedeebf741289784b5af60b402aa31aceb1a5
abufrank <riley.thornton1@gmail.com>
2018-08-15 15:02:59 -0400

    more service grid edits

--- frontpage-templates/frontpage-services.php
+++ frontpage-templates/frontpage-services.php
@@ -7,19 +7,19 @@
 		<!-- .grid-sizer empty element, only used for element sizing -->
 		<div class="grid-sizer"></div>
 		<div id="new-construction" class="grid-item grid-item--height2">
-			<h2>New Construction</h2>
+			<h2 class="service-header">New Construction</h2>
 		</div>
 		<div id="restoration" class="grid-item grid-item--width2">
-			<h2>Restoration and Renovation</h2>
+			<h2 class="service-header">Restoration and Renovation</h2>
 		</div>
 		<div id="detective" class="grid-item">
-			<h2>Architectural Detective Work</h2>
+			<h2 class="service-header">Architectural Detective Work</h2>
 		</div>
 		<div id="green" class="grid-item grid-item--height2">
-			<h2>Green Practices</h2>
+			<h2 class="service-header">Green Practices</h2>
 		</div>
 		<div id="purchase-consult" class="grid-item grid-item--width2">
-			<h2>Real Estate Purchase Consultation</h2>
+			<h2 class="service-header">Real Estate Purchase Consultation</h2>
 		</div>
 	</div> <!-- end masonry-layout -->
 </div>
