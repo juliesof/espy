ce6c8192a83382e77e963657a3bb37c0da071d23
juliesof <juliesof@gmail.com>
2018-08-16 19:15:48 +0200

    updates to hp services

--- frontpage-templates/frontpage-services.php
+++ frontpage-templates/frontpage-services.php
@@ -1,25 +1,53 @@
-<div id="homepage-services">
+<section id="homepage-services">
 	<div class="text-center">
 		<h2>Our Specialities</h2>
 	</div>
 	<!-- Begin Masonry Layout -->
-	<div id="services-grid" class="grid">
-		<!-- .grid-sizer empty element, only used for element sizing -->
-		<div class="grid-sizer"></div>
-		<div id="new-construction" class="grid-item grid-item--height2">
-			<h2 class="service-header">New Construction</h2>
-		</div>
-		<div id="restoration" class="grid-item grid-item--width2">
-			<h2 class="service-header">Restoration and Renovation</h2>
-		</div>
-		<div id="detective" class="grid-item">
-			<h2 class="service-header">Architectural Detective Work</h2>
-		</div>
-		<div id="green" class="grid-item grid-item--height2">
-			<h2 class="service-header">Green Practices</h2>
-		</div>
-		<div id="purchase-consult" class="grid-item grid-item--width2">
-			<h2 class="service-header">Real Estate Purchase Consultation</h2>
-		</div>
-	</div> <!-- end masonry-layout -->
-</div>
+	<main id="services-grid" class="grid">
+		
+		<article id="new-construction" class="grid-item grid-item--height2">
+			<div class="hp-services-inner-wrapper">
+				<h2 class="service-header">New Construction</h2>
+				<div>
+					<?php echo wp_get_attachment_image(11, full); ?>
+				</div>
+			</div>
+		</article>
+
+		<article id="restoration" class="grid-item grid-item--width2">
+			<div class="hp-services-inner-wrapper">
+				<h2 class="service-header">Restoration and Renovation</h2>
+				<div>
+					<img src="http://localhost:8888/espy/wp-content/uploads/2018/08/28ArlingtonKitchen2.jpg">
+				</div>
+			</div>
+		</article>
+
+		<article id="detective" class="grid-item grid-item--width1">
+			<div class="hp-services-inner-wrapper">
+				<h2 class="service-header">Architectural Detective Work</h2>
+				<div>
+					<?php echo wp_get_attachment_image(9, full); ?>
+				</div>
+			</div>
+		</article>
+
+		<article id="green" class="grid-item grid-item--height2">
+			<div class="hp-services-inner-wrapper">
+				<h2 class="service-header">Green Practices</h2>
+				<div>
+					<?php echo wp_get_attachment_image(13, full); ?>
+				</div>
+			</div>
+		</article>
+
+		<article id="purchase-consult" class="grid-item grid-item--width2">
+			<div class="hp-services-inner-wrapper">
+				<h2 class="service-header">Real Estate Purchase Consultation</h2>
+				<div>
+					<?php echo wp_get_attachment_image(10, full); ?>
+				</div>
+			</div>
+		</article>
+	</main> <!-- end services-grid -->
+</section>
--- js/creative.js
+++ js/creative.js
@@ -3,24 +3,21 @@
  *
  */
 jQuery( document ).ready(function( $ ){
-	//Homepage service grid
+	// //Homepage service grid
+	// var $servicesGrid = $('#services-grid')
+	// $servicesGrid.isotope({
+	// 	itemSelector:	'.grid-item',
+	// 	percentPosition: true,
+	// 	masonry: {
+	// 		columnWidth: '.grid-sizer'
+	// 	}
+	// }); // end homepage SERVICES function
 	var $servicesGrid = $('#services-grid')
 	$servicesGrid.isotope({
 		itemSelector:	'.grid-item',
-		percentPosition: true,
-		masonry: {
-			columnWidth: '.grid-sizer'
-		}
+		layoutMode: 'masonry'
 	}); // end homepage SERVICES function
 
-	//Homepage Gallery
-	// $('#homepage-gallery').isotope({
-	// 	itemSelector: '.hp-gallery-item',
-	// 	percentPosition: true,
-	// 	masonry: {
-	// 		columnWidth: '.gallery-sizer',
-	// 	}
-	// }); // end homepage GALLERIES function
 		$('#homepage-gallery').isotope({
 		itemSelector: '.hp-gallery-item',
 		layoutMode: 'masonry',
@@ -40,7 +37,7 @@ jQuery( document ).ready(function( $ ){
 
 	//toggle floating telephone icon number
 	var $tellyToggle = $('#tellyToggle');
-	$tellyToggle.on('click', function(){
+	$tellyToggle.on('hover', function(){
 		$tellyToggle.toggleClass('telephone-icon-small telephone-icon-expanded')
 	}); // end TELEPHONE-icon function
 
