250cf56ee4ea506f732da0bb59736c79a95d5c89
juliesof <juliesof@gmail.com>
2018-08-21 16:31:12 +0200

    update CTA bar text and color, update hero text and remove cta button

--- frontpage-templates/frontpage-cta.php
+++ frontpage-templates/frontpage-cta.php
@@ -1,4 +1,5 @@
 <div id="cta-bar" class="row flex-column justify-content-center align-items-center">
 	<h3 class="slogan">We'll help you write the next chapter</h3 class="slogan">
-	<div class="btn btn-primary cta-button">Schedule a Consultation</div>
+	<h1>Schedule a Consultation Today</h1>
+	<h3 class="slogan">413.695.6487</h3>
 </div> <!-- end cta bar -->
--- frontpage-templates/frontpage-hero.php
+++ frontpage-templates/frontpage-hero.php
@@ -1,6 +1,6 @@
 <div id="hero" class="row">	
 	<div class="header-content">
 		<h1 class="slogan">Every House Has a Story to Tell</h1>
-		<a href="/espy/gallery"><div id="hero-cta" class="btn btn-primary">Explore Our Specialties</div></a>
+		<a href="/espy/gallery">
 	</div>
 </div>
