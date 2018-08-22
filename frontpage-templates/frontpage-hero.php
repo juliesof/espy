23e2c86bb710f721b4ccd012baf253aa500bdc7c
abufrank <riley.thornton1@gmail.com>
2018-08-22 16:34:24 -0400

    added padding around portfolio gallery items and added animation and icon to frontpage scroll down icon

--- frontpage-templates/frontpage-hero.php
+++ frontpage-templates/frontpage-hero.php
@@ -2,9 +2,9 @@
 	<div class="header-content">
 		<h1 class="slogan">Every House Has a Story to Tell</h1>
 	</div>
-	<a href="#frontpage-welcome">
+	<a href="#frontpage-welcome" class="bounce">
 		<div class="scroll-icon">
-			
+			<i class="fa fa-2x fa-angle-down"></i>
 		</div>
 	</a>
 </div>
