b4eba3f086fbf4205bf6cf52fa1d97cf79ab5d64
juliesof <juliesof@gmail.com>
2018-08-30 23:21:03 +0200

    layout for custom template/alternating blocks template

--- single-custom.php
+++ single-custom.php
@@ -14,9 +14,9 @@ $container   = 'container-fluid';
 
 <div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
 
-	<div class="row">
+<!-- 	<div class="row">-->
 
-		<main class="site-main" id="main">
+		<main class="site-main" id="main"> 
 
 			<?php 
 
@@ -27,52 +27,60 @@ $container   = 'container-fluid';
 					<?php the_title( '<h1>', '</h1>' ); ?>
 				</header><!-- .entry-header -->
 
-					
+				<?php 
+					if( have_rows('template_row') ): 
+						while( have_rows('template_row') ) : the_row();
+
+						$text = get_sub_field('text_block');
+						$image = get_sub_field('image');
+				?>
 					<section class="row block-row">
-						<div class="col-lg-6 block-element block-1">
-							test 1
+						<div class="col-lg-6 block-element ">
+							
 						</div>
-						<div class="col-lg-6 block-element block-2">
-							test 2
+						<div class="col-lg-6 block-element">
+							
 						</div>
 					</section>
 
 					<section class="row block-row">
-							<div class="col-lg-6 block-element block-1">
-								test 1
+							<div class="col-lg-6 block-element ">
+								
 							</div>
-							<div class="col-lg-6 block-element block-2">
-								test 2
+							<div class="col-lg-6 block-element">
+								
 							</div>
 						
 					</section>
 
 					<section class="row block-row">
-							<div class="col-lg-6 block-element block-1">
-								test 1
+							<div class="col-lg-6 block-element ">
+								
 							</div>
-							<div class="col-lg-6 block-element block-2">
-								test 2
+							<div class="col-lg-6 block-element">
+								
 							</div>
 				
 					</section>
 
 					<section class="row block-row">
-							<div class="col-lg-6 block-element block-1">
-								test 1
+							<div class="col-lg-6 block-element ">
+								
 							</div>
-							<div class="col-lg-6 block-element block-2">
-								test 2
+							<div class="col-lg-6 block-element">
+								
 							</div>
 		
 					</section>
+
+				<?php endwhile; endif; ?>
 		
 
 			<?php endwhile; // end of the loop. ?>
 
 		</main><!-- #main -->
 
-</div><!-- .row -->
+<!-- </div> --><!-- .row -->
 
 </div><!-- Container end -->
 
